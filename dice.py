30f84e0f3cdbc55c78913b2dfc2651f622d4f93c
a <a@b>
2014-04-03 23:32:00 +0000

    merp. it's now nice code.

--- dice.py
+++ dice.py
@@ -8,5 +8,5 @@ if sides < 2:
 	while side < 2:
 		print("Give me a valid number of sides, please.")
 		sides = int_input("How many sides should the die have? ")
-print(randint(1,6))
 
+print(randint(1, sides))
