bab9ae07dcf8b95220c6e3ee5c0b978b57fba33e
a <a@b>
2014-04-03 23:31:32 +0000

    yey. added some sidey stuff... not complete

--- dice.py
+++ dice.py
@@ -1,5 +1,12 @@
 #!/usr/bin/env python3
 
+from inppy  import *
 from random import randint
 
+sides = int_input("How many sides should the die have? ")
+if sides < 2:
+	while side < 2:
+		print("Give me a valid number of sides, please.")
+		sides = int_input("How many sides should the die have? ")
 print(randint(1,6))
+
