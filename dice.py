d581f0cbe97d7de3e9bde08fee01e45d396fd5d2
a <a@b>
2014-04-03 23:36:37 +0000

    should ask for amount of dice and work now

--- dice.py
+++ dice.py
@@ -3,10 +3,17 @@
 from inp    import *
 from random import randint
 
+dice  = int_input("How many dice do ya want? ")
+if dice < 1:
+	while dice < 1:
+		print("Give me a valid number of dice, please.")
+		dice = int_input("How many dice do ya want? ")
+
 sides = int_input("How many sides should the die have? ")
 if sides < 2:
 	while sides < 2:
 		print("Give me a valid number of sides, please.")
 		sides = int_input("How many sides should the die have? ")
 
-print(randint(1, sides))
+for None in dice:
+	print(randint(1, sides))
